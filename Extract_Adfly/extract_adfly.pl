17e976a8f774c791867719516a99c7c7dac34b5e
Dylan Durdle <ddurdle@gmail.com>
2016-01-28 22:07:44 -0500

    new

---
+++ Extract_Adfly/extract_adfly.pl
@@ -0,0 +1,21 @@
+######
+# Extract URLs from a txt file
+#  Parameters
+###
+
+use Getopt::Std;		# and the getopt module
+
+my %opt;
+die (USAGE) unless (getopts ('u:',\%opt));
+
+my $URL = $opt{'u'};
+
+
+require 'crawler.pm';
+@results = TOOLS_CRAWLER::complexGET($URL,'test.txt',['drive.google.com'],[],[('https://drive.google.com/file/d/', '/', '/')]);
+if ($#results >= 3 ){
+	print STDERR 'Google Drive document = ' .  $results[3]. "\n";
+}
+
+
+exit(0);
--- Extract_URLS/extract_urls.pl
+++ Extract_URLS/extract_urls.pl
@@ -6,16 +6,19 @@
 use Getopt::Std;		# and the getopt module
 
 my %opt;
-die (USAGE) unless (getopts ('f:',\%opt));
+die (USAGE) unless (getopts ('f:F:',\%opt));
 
 my $file = $opt{'f'};
+my $filter = $opt{'F'};
 
 
 open(LIST, './'.$file) or die ('cannot open input'.$file);
 
 while(my $line = <LIST>){
-	if ($line =~ m%http[^\:]?\:\/\/[^\s]+%){
+	if ($line =~ m%http[^\:]?\:\/\/[^\s]+% and $line =~ m%$filter%){
 		my ($URL) = $line =~ m%(http[^\:]?\:\/\/[^\s]+)%;
+		$URL =~ s%"%%gi;
+		$URL =~ s%i%%gi;
 		print STDOUT "$URL\n";
 	}
 }
