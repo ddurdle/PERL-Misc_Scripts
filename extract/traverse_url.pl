ff965201b5347edd1a19544745e6772c3f1a5b95
Dylan Durdle <ddurdle@gmail.com>
2016-02-01 08:28:26 -0500

    add last result fetch

--- extract/traverse_url.pl
+++ extract/traverse_url.pl
@@ -6,18 +6,22 @@
 use Getopt::Std;		# and the getopt module
 
 my %opt;
-die (USAGE) unless (getopts ('u:1:2:3:',\%opt));
+die (USAGE) unless (getopts ('u:1:2:3:l',\%opt));
 
 my $URL = $opt{'u'};
 my $searchCriteria = quotemeta $opt{'1'};
 my $extractStart = quotemeta $opt{'2'};
 my $extractEnd = quotemeta $opt{'3'};
+my $lastOnly = 0;
+if (defined($opt{'l'})){
+	$lastOnly = 1;
+}
 
 
 require 'crawler.pm';
 @results = TOOLS_CRAWLER::complexGET($URL,undef,[],[],[($searchCriteria, $extractStart, $extractEnd)]);
-if ($#results >= 3 ){
-	print STDOUT 'Found = ' .  $results[3]. "\n";
+for (my $i=3; $i <=$#results; $i = $i+2){
+	print STDOUT 'Found = ' .  $results[$i]. "\n" if (not $lastOnly or $i == $#results);
 }
 
 
