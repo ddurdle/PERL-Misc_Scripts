819e04cb06014a458b05275b28c8e041423b32b7
Dylan Durdle <ddurdle@gmail.com>
2016-04-16 23:53:23 -0400

    new routine for finding a value on a web page

---
+++ extract/find_value.pl
@@ -0,0 +1,39 @@
+######
+# Find a value on a url
+#  Parameters
+#  -u url
+#  -s string to grep value from (value = %1%)
+###
+
+use Getopt::Std;		# and the getopt module
+
+my %opt;
+die (USAGE) unless (getopts ('u:1:2:3:l',\%opt));
+
+my $URL = $opt{'u'};
+my $searchCriteria = quotemeta $opt{'1'};
+my $extractStart = quotemeta $opt{'2'};
+my $extractEnd = quotemeta $opt{'3'};
+
+if ($URL eq ''){
+	$URL = <>;
+}
+
+#my $extractStart = quotemeta $opt{'2'};
+
+my $lastOnly = 0;
+if (defined($opt{'l'})){
+	$lastOnly = 1;
+}
+
+require 'crawler.pm';
+
+@results = TOOLS_CRAWLER::complexGET($URL,undef,[],[],[($searchCriteria, $extractStart, $extractEnd)]);
+for (my $i=3; $i <=$#results; $i = $i+2){
+	print STDOUT 'Found = ' .  $results[$i]. "\n" if (not $lastOnly or $i == $#results);
+}
+
+
+
+
+exit(0);
