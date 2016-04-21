2d266d25ba6e71b8f043ab79fa2d69ced2867c55
Dylan Durdle <ddurdle@gmail.com>
2016-04-21 10:40:06 -0400

    add print dbm

---
+++ dbm/print_dbm.pl
@@ -0,0 +1,96 @@
+
+##
+#
+# This script is used to debug a dbm file.
+# -d dbm file (such as -d gd.uername.md5.db)
+# optional:
+#  -p   -- prints dbm contents
+#  -c  -- count the number of enteries
+#
+###
+
+package PDRIVE;
+
+
+
+use strict;
+
+use constant DEBUG => 0;
+use constant DEBUG_LOG => 'debug.log';
+
+
+
+use Getopt::Std;
+use constant USAGE => " usage: $0 [-d dbm]\n";
+
+my %opt;
+die (USAGE) unless (getopts ('d:pc',\%opt));
+my $dbm_file = $opt{d};
+
+&PDRIVE::DBM::init($dbm_file);
+
+if ($opt{p}){
+	&PDRIVE::DBM::printDBHash();
+}elsif ($opt{c}){
+	&PDRIVE::DBM::countDBHash();
+}
+
+{
+package PDRIVE::DBM;
+
+use DB_File;
+use Fcntl;
+
+use strict;
+
+my $dbm;
+my %dbase;
+sub init($){
+
+ $dbm = shift;
+
+}
+
+
+
+
+sub printDBHash(){
+
+  print "Database $dbm consists of the following key value pairs...\n";
+
+
+
+  tie(%dbase, 'DB_File', $dbm,O_RDWR|O_CREAT, 0666) or die "can't open $dbm: $!";
+
+  foreach my $key (keys %dbase) {
+    print "$key: $dbase{$key}\n";
+  }
+
+
+  untie(%dbase);
+
+}
+
+
+
+sub countDBHash(){
+
+  print "Database $dbm contains this count...\n";
+
+
+  tie(%dbase, 'DB_File', $dbm,O_RDWR|O_CREAT, 0666) or die "can't open $dbm: $!";
+
+  my $count = 0;
+  foreach my $key (keys %dbase) {
+    $count++;
+  }
+
+  untie(%dbase);
+
+  print "Number of enteries = " . $count ."\n";
+
+}
+
+1;
+
+}
