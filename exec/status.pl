478747fab189f69137503153d49869d6bea157c9
Dylan Durdle <ddurdle@gmail.com>
2016-04-22 11:44:50 -0400

    addsome functionality to status

--- exec/status.pl
+++ exec/status.pl
@@ -4,33 +4,41 @@
 #  -c command to run
 #  -s success criteria
 #  -f failure criteria
-#  -r (retry on failure)
+#  -r count
+
 ###
 
 use Getopt::Std;		# and the getopt module
-
+use constant USAGE => '';
 my %opt;
-die (USAGE) unless (getopts ('c:s:rf:',\%opt));
+die (USAGE) unless (getopts ('c:s:r:f:S:F:',\%opt));
 
 my $command = $opt{'c'};
 my $success = $opt{'s'};
 my $failure = $opt{'f'};
-my $isRetry = $opt{'r'};
+my $retryCount = $opt{'r'};
+my $failureCmd = $opt{'F'};
+my $successCmd = $opt{'S'};
 
-while(1){
+my $isFailure=0;
+for (my$i=0;$i<= $retryCount; $i++){
 	my $ret = `$command`;
 	if ($success ne '' and $ret =~ m%\Q$success\E%){
-		exit 0;
+		$isFailure=0;
 		last;
 	}elsif ($failure ne '' and $ret =~ m%\Q$failure\E%){
-		exit 1;
+		$isFailure=1;
 	}elsif ($failure ne '' and $success eq '' ){
-		exit 1;
+		$isFailure=0;
 		last;
 	}else{
 		print 'output = '.$ret;
-		exit 0;
-		last;
+		$isFailure=0;
 	}
+}
+if ($isFailure){
+	`$failureCmd`;
+}else{
+	`$successCmd`;
 }
 
