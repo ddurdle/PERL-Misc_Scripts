0936d30d6009c778680895b4c3b19b088c689b62
Dylan Durdle <ddurdle@gmail.com>
2016-04-23 23:37:30 -0400

    add multiple value fetch

--- extract/find_value.pl
+++ extract/find_value.pl
@@ -8,7 +8,7 @@
 use Getopt::Std;		# and the getopt module
 
 my %opt;
-die (USAGE) unless (getopts ('u:1:2:3:lh:',\%opt));
+die (USAGE) unless (getopts ('u:1:2:3:4:5:6:lh:',\%opt));
 
 my $URL = $opt{'u'};
 my $headers = $opt{'h'};
@@ -16,6 +16,10 @@ my $headers = $opt{'h'};
 my $searchCriteria = quotemeta $opt{'1'};
 my $extractStart =  quotemeta  $opt{'2'};#
 my $extractEnd = quotemeta $opt{'3'};#quotemeta $opt{'3'};
+my $searchCriteria2 = quotemeta $opt{'4'};
+my $extractStart2 =  quotemeta  $opt{'5'};#
+my $extractEnd2 = quotemeta $opt{'6'};#quotemeta $opt{'3'};
+
 
 
 #my $extractStart = quotemeta $opt{'2'};
@@ -41,7 +45,12 @@ if ($URL eq ''){
 
 while ($URL ne ''){
 
-	@results = TOOLS_CRAWLER::complexGET($URL,undef,[],[],[($searchCriteria, $extractStart, $extractEnd)]);
+	if ($searchCriteria2 ne '' ){
+		@results = TOOLS_CRAWLER::complexGET($URL,undef,[],[],[($searchCriteria, $extractStart, $extractEnd),($searchCriteria2, $extractStart2, $extractEnd2)]);
+
+	}else{
+		@results = TOOLS_CRAWLER::complexGET($URL,undef,[],[],[($searchCriteria, $extractStart, $extractEnd)]);
+	}
 	for (my $i=3; $i <=$#results; $i = $i+2){
 		next if $results[$i] eq $URL;
 		print STDOUT 'Found = ' .  $results[$i]. "\n" if (not $lastOnly or $i == $#results);
