7fbf05d3a8b375746294de01475300c42fd2d930
Dylan Durdle <ddurdle@gmail.com>
2016-04-25 13:32:37 -0400

    sync

--- grep/replace.pl
+++ grep/replace.pl
@@ -3,17 +3,20 @@
 #  Parameters
 #  -g string to look for
 #  -r replacement string
+#  -R replacement end of string
 ###
 
 use Getopt::Std;		# and the getopt module
 
 my %opt;
-die (USAGE) unless (getopts ('f:g:r:',\%opt));
+die (USAGE) unless (getopts ('f:g:r:R:',\%opt));
 
 my $filter = $opt{'g'};
+#my $filterEnd = $opt{'G'};
 my $file = $opt{'f'};
 
-my $replacement = $opt{'r'};#quotemeta $opt{'F'};
+my $replacement = $opt{'r'};#quotemeta $opt{'r'};
+my $replacementEnd = $opt{'R'};#quotemeta $opt{'r'};
 
 if ($file ne ''){
 	open(LIST, $file) or die ('cannot open input '.$file);
@@ -22,7 +25,7 @@ if ($file ne ''){
 }
 
 while(my $line = <LIST>){
-	$line =~ s%$filter%$replacement%g;
+	$line =~ s%$filter%$replacement.($1<10?'0'.$1:$1).$replacementEnd%ge;
 	print STDOUT $line;
 
 }
