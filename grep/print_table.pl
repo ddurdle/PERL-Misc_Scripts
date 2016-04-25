d0d2cda619aae91300d77ca169a86e3779f6ecec
Dylan Durdle <ddurdle@gmail.com>
2016-04-25 13:44:48 -0400

    sync

---
+++ grep/print_table.pl
@@ -0,0 +1,18 @@
+######
+# Extract data into a table
+#  Parameters
+#  -f file containing data
+#  -1 start grep to find against data
+#  -2 end grep to find against data
+#    command to run: <1><f><2>
+###
+
+use Getopt::Std;		# and the getopt module
+
+my %opt;
+die (USAGE) unless (getopts ('1:2:',\%opt));
+
+my $value1 = $opt{'1'};
+my $value2 = $opt{'2'};
+
+print STDOUT $value1 . "\t" .$value2 . "\n";
