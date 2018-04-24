2110a870783d0c6a919f88815a720dd48dc98af6
Dylan Durdle <ddurdle@gmail.com>
2018-04-24 19:57:45 -0400

    tesitng

--- ffmpeg/ffmpeg.pl
+++ ffmpeg/ffmpeg.pl
@@ -48,6 +48,7 @@ if ($duration_ptr == -1){
 	while ($retry< RETRY and $retry > 0){
 		my $output = 'x';
 		$pid = open LS, '/u01/ffmpeg-git-20171123-64bit-static/ffmpeg $arglist -v error 2>&1 |' or die$!;
+		print "pid = $pid\n";
 		close LS;
 		#my $output = `/u01/ffmpeg-git-20171123-64bit-static/ffmpeg $arglist -v error 2>&1`;
 		if($output ne ''){
