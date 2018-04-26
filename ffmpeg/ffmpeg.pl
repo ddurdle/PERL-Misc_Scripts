3ec23138eaa2930d4cfdcd9abde54a88aee2506e
Dylan Durdle <ddurdle@gmail.com>
2018-04-26 15:58:24 -0400

    add more hanlders

--- ffmpeg/ffmpeg.pl
+++ ffmpeg/ffmpeg.pl
@@ -8,6 +8,11 @@ my $pidi=0;
 $SIG{QUIT} = sub {  kill 'KILL', $pid;die "Caught a quit $pid $!"; };
 $SIG{TERM} = sub {  kill 'KILL', $pid;die "Caught a term $pid $!"; };
 $SIG{INT} = sub {  kill 'KILL', $pid;die "Caught a int $pid $!"; };
+$SIG{HUP} = sub {  kill 'KILL', $pid;die "Caught a hup $pid $!"; };
+$SIG{ABRT} = sub {  kill 'KILL', $pid;die "Caught a abrt $pid $!"; };
+$SIG{TRAP} = sub {  kill 'KILL', $pid;die "Caught a trap $pid $!"; };
+$SIG{STOP} = sub {  kill 'KILL', $pid;die "Caught a stop $pid $!"; };
+
 
 sub createArglist(){
 	my $arglist = '';
