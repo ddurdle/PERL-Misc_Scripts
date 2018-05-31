299273220bed69ecf1b041c696e09e494a92ed06
Dylan Durdle <ddurdle@gmail.com>
2018-05-31 13:22:57 -0400

    testing

--- ffmpeg/PlexTranscoder.pl
+++ ffmpeg/PlexTranscoder.pl
@@ -93,6 +93,18 @@ if ($isSRT){
 		print STDERR "running " . 'ffmpeg ' . $arglist . "\n";
 
 		`$PATH_TO_TRANSCODER $arglist`;
+
+		$pid = open ( LS, '-|', $$PATH_TO_TRANSCODER . ' ' . $arglist . ' 2>&1');
+		my $output = do{ local $/; <LS> };
+		close LS;
+		#my $output = `/u01/ffmpeg-git-20171123-64bit-static/ffmpeg $arglist -v error 2>&1`;
+		print $output;
+		# no transcoding available
+		if($output =~ m%moov atom not found%){
+			$arglist =~ s%\-f mp4 %\-f matroska,webm %;
+			`$FFMPEG_OEM $arglist`;
+		}
+
 	}
 
 # ### Python-GoogleDrive-VideoStream REQUEST
