b5c504d4218fce1ac56bb645c0608cef10985505
Dylan Durdle <ddurdle@gmail.com>
2018-05-31 13:06:22 -0400

    fix

--- ffmpeg/PlexTranscoder.pl
+++ ffmpeg/PlexTranscoder.pl
@@ -17,7 +17,7 @@ use constant GOOGLE_TRANSCODE => 1;
 # prefer to direct stream requests with Google Transcode feeds (will reduce CPU load)
 use constant PREFER_GOOGLE_TRANSCODE => 1;
 
-use constant PATH_TO_TRANSCODER => '/usr/lib/plexmediaserver/Plex Transcoder.oem';
+use constant PATH_TO_TRANSCODER => '"/usr/lib/plexmediaserver/Plex Transcoder.oem"';
 
 
 my $pidi=0;
