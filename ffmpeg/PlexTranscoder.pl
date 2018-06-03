4a1afd4944ac040f8c6eb1d1546e584047f50c1c
Dylan Durdle <ddurdle@gmail.com>
2018-06-02 20:50:45 -0400

    testing

--- ffmpeg/PlexTranscoder.pl
+++ ffmpeg/PlexTranscoder.pl
@@ -90,10 +90,10 @@ if (PREFER_GOOGLE_TRANSCODE){
 	}elsif ($arglist =~ m%scale\=w\=720\:h\=406%){
 		$video .= '&preferred_quality=2&override=true';
 	}elsif ($arglist =~ m%scale\=w\=1920\:h\=1080%){
-		$video .= '&preferred_quality=2&override=true';
+		$video .= '&preferred_quality=0&override=true';
 
 	}else{
-		#$video .= '&preferred_quality=0&override=true';
+		$video .= '&preferred_quality=0&override=true';
 	}
 }
 if ($arglist =~ m% dash %){
