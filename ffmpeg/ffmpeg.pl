6399ba714ca39d058b19b97e43537d79f0316d24
Dylan Durdle <ddurdle@gmail.com>
2018-06-14 22:41:04 -0400

    testing

--- ffmpeg/ffmpeg.pl
+++ ffmpeg/ffmpeg.pl
@@ -125,6 +125,9 @@ if ($isSRT){
 			$arglist =~ s%\"?\Q$url\E\"?%\"$url\&preferred_quality\=0\&override\=true\"%;
 		}
 
+		$arglist =~ s%\-map 0\:2 %-i "/u01/recordings/test3.aac" \-map 0\:2 %;
+
+
 		$arglist =~ s%\-f matroska,webm %\-f mp4 %;
 
 		print STDERR "URL = $url, $arglist\n";
